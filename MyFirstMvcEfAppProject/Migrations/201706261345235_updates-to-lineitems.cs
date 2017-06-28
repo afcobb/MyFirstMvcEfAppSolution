36b7005aecea6e05e797436a9a607f4639a67580
Alex <afcobb@gmail.com>
2017-06-28 09:46:40 -0400

    added about to system service"

---
+++ MyFirstMvcEfAppProject/Migrations/201706261345235_updates-to-lineitems.cs
@@ -0,0 +1,16 @@
+namespace MyFirstMvcEfAppProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatestolineitems : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
